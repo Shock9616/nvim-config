f237cecd492b1102102b64543172e45a2e3f622b
Shock9616 <Shock9616@users.noreply.github.com>
2023-09-10 00:30:02 -0700

    Installed treesitter context

--- lua/user/plugins/treesitter.lua
+++ lua/user/plugins/treesitter.lua
@@ -1,26 +1,33 @@
 --
 -- treesitter.lua
 --
--- Better syntax highlighting
+-- Better syntax highlighting/code context
 --
 
 return {
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-    opts = {
-        highlight = {
-            enable = true,
-        },
-        ensure_installed = {
-            "lua",
-            "vim",
-            "vimdoc",
-            "python",
-            "rust",
-            "swift",
-            "c",
-            "c++",
-            "bash",
-        }
-    }
+	{
+		"nvim-treesitter/nvim-treesitter",
+		build = ":TSUpdate",
+		opts = {
+			highlight = {
+				enable = true,
+			},
+			ensure_installed = {
+				"lua",
+				"vim",
+				"vimdoc",
+				"python",
+				"rust",
+				"swift",
+				"c",
+				"c++",
+				"bash",
+			},
+		},
+	},
+
+	{
+		"nvim-treesitter/nvim-treesitter-context",
+		opts = {},
+	},
 }
