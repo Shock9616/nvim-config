ec2cd749cf0dd631d2540a7617c77db6ff5a8970
Shock9616 <Shock9616@users.noreply.github.com>
2023-09-10 00:23:24 -0700

    Reformatted cmp menu/added cmp keymaps

--- lua/user/plugins/lsp-zero.lua
+++ lua/user/plugins/lsp-zero.lua
@@ -46,8 +46,48 @@ return {
 		local cmp = require("cmp")
 		local luasnip = require("luasnip")
 
+		local kind_icons = {
+			Text = "",
+			Method = "m",
+			Function = "",
+			Constructor = "",
+			Field = "",
+			Variable = "",
+			Class = "",
+			Interface = "",
+			Module = "",
+			Property = "",
+			Unit = "",
+			Value = "",
+			Enum = "",
+			Keyword = "",
+			Snippet = "",
+			Color = "",
+			File = "",
+			Reference = "",
+			Folder = "",
+			EnumMember = "",
+			Constant = "",
+			Struct = "",
+			Event = "",
+			Operator = "",
+			TypeParameter = "",
+		}
+
 		cmp.setup({
+			snippet = {
+				expand = function(args)
+					luasnip.lsp_expand(args.body) -- For `luasnip` users.
+				end,
+			},
 			mapping = cmp.mapping.preset.insert({
+				["<C-b>"] = cmp.mapping.scroll_docs(-4),
+				["<C-f>"] = cmp.mapping.scroll_docs(4),
+				["<C-Space>"] = cmp.mapping.complete(),
+				["<C-e>"] = cmp.mapping.abort(),
+				["<CR>"] = cmp.mapping.confirm({ select = true }),
+
+				-- Supertab
 				["<Tab>"] = cmp.mapping(function(fallback)
 					if cmp.visible() then
 						cmp.select_next_item()
@@ -70,6 +110,41 @@ return {
 					end
 				end, { "i", "s" }),
 			}),
+
+			formatting = {
+				fields = { "kind", "abbr", "menu" },
+				format = function(entry, vim_item)
+					-- Kind icons
+					vim_item.kind = string.format("%s", kind_icons[vim_item.kind])
+					-- vim_item.kind = string.format('%s %s', kind_icons[vim_item.kind], vim_item.kind) -- This concatonates the icons with the name of the item kind
+					vim_item.menu = ({
+						nvim_lsp = "[LSP]",
+						nvim_lua = "[NVIM_LUA]",
+						luasnip = "[Snippet]",
+						buffer = "[Buffer]",
+						path = "[Path]",
+					})[entry.source.name]
+					return vim_item
+				end,
+			},
+			sources = {
+				{ name = "nvim_lsp" },
+				{ name = "nvim_lua" },
+				{ name = "luasnip" },
+				{ name = "buffer" },
+				{ name = "path" },
+			},
+			confirm_opts = {
+				behavior = cmp.ConfirmBehavior.Replace,
+				select = false,
+			},
+			window = {
+				documentation = cmp.config.window.bordered(),
+			},
+			experimental = {
+				ghost_text = true,
+				native_menu = false,
+			},
 		})
 
 		local diagnostics_signs = {
