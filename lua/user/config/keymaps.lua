5595b30f4a6667f94fb404299f9b9893fab68169
Shock9616 <Shock9616@users.noreply.github.com>
2023-09-10 22:50:51 -0700

    Remapped 'U' to be redo

--- lua/user/config/keymaps.lua
+++ lua/user/config/keymaps.lua
@@ -8,6 +8,9 @@
 vim.keymap.set({ "n", "x" }, "j", "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
 vim.keymap.set({ "n", "x" }, "k", "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
 
+-- Better redo
+vim.keymap.set("n", "U", "<C-r>", { desc = "Redo" })
+
 -- Move to window using the <ctrl> hjkl keys
 vim.keymap.set("n", "<C-h>", "<C-w>h", { desc = "Go to left window", remap = true })
 vim.keymap.set("n", "<C-j>", "<C-w>j", { desc = "Go to lower window", remap = true })
