0e9592cdc82c6f831a8839a9f9cd6b2db047b400
Shock9616 <Shock9616@users.noreply.github.com>
2023-10-03 16:47:34 -0700

    Improved lualine style

--- lua/user/plugins/lualine.lua
+++ lua/user/plugins/lualine.lua
@@ -7,38 +7,77 @@
 return {
 	"nvim-lualine/lualine.nvim",
 	dependencies = { "nvim-tree/nvim-web-devicons" },
-	opts = {
-		options = {
-			theme = "ayu_mirage",
-			component_separators = "|",
-			section_separators = { left = "", right = "" },
-		},
-		sections = {
-			lualine_a = {
-				{ "mode", separator = { left = "", right = "" } },
+	config = function()
+		local clients_lsp = function()
+			local bufnr = vim.api.nvim_get_current_buf()
+
+			local clients = vim.lsp.buf_get_clients(bufnr)
+			if next(clients) == nil then
+				return ""
+			end
+
+			local c = {}
+			for _, client in pairs(clients) do
+				table.insert(c, client.name)
+			end
+			return "  " .. table.concat(c, "|")
+		end
+
+		local custom_ayu_mirage = require("lualine.themes.ayu_mirage")
+
+		-- Custom colours
+		custom_ayu_mirage.normal.b.bg = "#343d52"
+
+		require("lualine").setup({
+			options = {
+				theme = custom_ayu_mirage,
+				component_separators = "",
+				section_separators = { left = "", right = "" },
 			},
-			lualine_b = { "filename", "branch" },
-			lualine_c = { "diff" },
-			lualine_x = {
-				{
-					"diagnostics",
-					symbols = { error = " ", warn = " ", info = " ", hint = "" },
-					update_in_insert = true,
+			sections = {
+				lualine_a = {
+					{ "mode", separator = { left = "█", right = "" } },
+				},
+				lualine_b = {
+					{
+						"filetype",
+						icon_only = true,
+						padding = { left = 1, right = 0 },
+					},
+					"filename",
+				},
+				lualine_c = {
+					{
+						"branch",
+						icon = "",
+					},
+					{
+						"diff",
+						symbols = { added = " ", modified = " ", removed = " " },
+						colored = false,
+					},
+				},
+				lualine_x = {
+					{
+						"diagnostics",
+						symbols = { error = " ", warn = " ", info = " ", hint = "" },
+						update_in_insert = true,
+					},
+				},
+				lualine_y = { clients_lsp },
+				lualine_z = {
+					{ "location", separator = { left = "", right = "█" }, icon = "" },
 				},
 			},
-			lualine_y = { "filetype" },
-			lualine_z = {
-				{ "location", separator = { left = "", right = "" } },
+			inactive_sections = {
+				lualine_a = { "filename" },
+				lualine_b = {},
+				lualine_c = {},
+				lualine_x = {},
+				lualine_y = {},
+				lualine_z = { "location" },
 			},
-		},
-		inactive_sections = {
-			lualine_a = { "filename" },
-			lualine_b = {},
-			lualine_c = {},
-			lualine_x = {},
-			lualine_y = {},
-			lualine_z = { "location" },
-		},
-		extensions = { "toggleterm", "trouble" },
-	},
+			extensions = { "toggleterm", "trouble" },
+		})
+	end,
 }
