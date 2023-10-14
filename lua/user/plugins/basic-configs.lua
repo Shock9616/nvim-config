b2f5951f4d9f629b22a44a7197917de1e3a129e3
Shock9616 <Shock9616@users.noreply.github.com>
2023-10-14 16:17:25 -0700

    Re-install ultimate-autopairs

--- lua/user/plugins/basic-configs.lua
+++ lua/user/plugins/basic-configs.lua
@@ -5,88 +5,86 @@
 --
 
 return {
-	{
-		-- Better commenting
-		"numToStr/Comment.nvim",
-		opts = {},
-		lazy = false,
-	},
-	{
-		-- LSP Notifications
-		"j-hui/fidget.nvim",
-		tag = "legacy",
-		event = "LspAttach",
-		opts = {},
-	},
-	{
-		-- Git gutter
-		"lewis6991/gitsigns.nvim",
-		event = { "BufReadPre", "BufNewFile" },
-		opts = {},
-	},
-	{
-		"lukas-reineke/headlines.nvim",
-		dependencies = "nvim-treesitter/nvim-treesitter",
-		ft = "markdown",
-		opts = {},
-	},
-	{
-		-- Indent guides
-		"lukas-reineke/indent-blankline.nvim",
-		config = function()
-			require("ibl").setup({
-				indent = { char = "│" },
-			})
-		end,
-	},
-	{
-		-- Live preview markdown files in browser
-		"iamcco/markdown-preview.nvim",
-		config = function()
-			vim.fn["mkdp#util#install"]()
-		end,
-		ft = "markdown",
-	},
-	{
-		"windwp/nvim-autopairs",
-		event = "InsertEnter",
-		opts = {
-			check_ts = true,
-		},
-	},
-	{
-		"stevearc/oil.nvim",
-		config = function()
-			require("oil").setup()
-		end,
-		-- Optional dependencies
-		dependencies = { "nvim-tree/nvim-web-devicons" },
-	},
-	{
-		-- Discord rich presence because why the heck not?
-		"andweeb/presence.nvim",
-		opts = {},
-	},
-	{
-		-- Better Rust support
-		"simrat39/rust-tools.nvim",
-		ft = "rust",
-	},
-	{
-		-- Epic fuzzy finder
-		"nvim-telescope/telescope.nvim",
-		tag = "0.1.2",
-		dependencies = { "nvim-lua/plenary.nvim" },
-		opts = {},
-	},
-	{
-		"akinsho/toggleterm.nvim",
-		version = "*",
-		config = true,
-	},
-	{
-		-- Pretty diagnostics list
-		"folke/trouble.nvim",
-		opts = {},
-	},
+    {
+        -- Better commenting
+        "numToStr/Comment.nvim",
+        opts = {},
+        lazy = false,
+    },
+    {
+        -- LSP Notifications
+        "j-hui/fidget.nvim",
+        tag = "legacy",
+        event = "LspAttach",
+        opts = {},
+    },
+    {
+        -- Git gutter
+        "lewis6991/gitsigns.nvim",
+        event = { "BufReadPre", "BufNewFile" },
+        opts = {},
+    },
+    {
+        "lukas-reineke/headlines.nvim",
+        dependencies = "nvim-treesitter/nvim-treesitter",
+        ft = "markdown",
+        opts = {},
+    },
+    {
+        -- Indent guides
+        "lukas-reineke/indent-blankline.nvim",
+        config = function()
+            require("ibl").setup({
+                indent = { char = "│" },
+            })
+        end,
+    },
+    {
+        -- Live preview markdown files in browser
+        "iamcco/markdown-preview.nvim",
+        config = function()
+            vim.fn["mkdp#util#install"]()
+        end,
+        ft = "markdown",
+    },
+    {
+        "stevearc/oil.nvim",
+        config = function()
+            require("oil").setup()
+        end,
+        -- Optional dependencies
+        dependencies = { "nvim-tree/nvim-web-devicons" },
+    },
+    {
+        -- Discord rich presence because why the heck not?
+        "andweeb/presence.nvim",
+        opts = {},
+    },
+    {
+        -- Better Rust support
+        "simrat39/rust-tools.nvim",
+        ft = "rust",
+    },
+    {
+        -- Epic fuzzy finder
+        "nvim-telescope/telescope.nvim",
+        tag = "0.1.2",
+        dependencies = { "nvim-lua/plenary.nvim" },
+        opts = {},
+    },
+    {
+        "akinsho/toggleterm.nvim",
+        version = "*",
+        config = true,
+    },
+    {
+        -- Pretty diagnostics list
+        "folke/trouble.nvim",
+        opts = {},
+    },
+    {
+        "altermo/ultimate-autopair.nvim",
+        event = { "InsertEnter", "CmdlineEnter" },
+        opts = {},
+    },
 }
