9651944fae9bff6f52d723b2756f3add3dbb8f1b
Shock9616 <Shock9616@users.noreply.github.com>
2023-10-17 11:11:41 -0700

    Re-add nvim-notify

--- lua/user/plugins/noice.lua
+++ lua/user/plugins/noice.lua
@@ -7,24 +7,29 @@
 return {
 	"folke/noice.nvim",
 	event = "VeryLazy",
-	opts = {
-		lsp = {
-			override = {
-				["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-				["vim.lsp.util.stylize_markdown"] = true,
-				["cmp.entry.get_documentation"] = true,
+	config = function()
+		require("noice").setup({
+			lsp = {
+				override = {
+					["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+					["vim.lsp.util.stylize_markdown"] = true,
+					["cmp.entry.get_documentation"] = true,
+				},
 			},
-		},
-		presets = {
-			bottom_search = true, -- Use bottom cmdline for search
-			command_palette = true, -- Position the cmdline and popupmenu together
-			long_message_to_split = true, -- Long messages will be sent to a split
-			inc_rename = false,
-			lsp_doc_border = false,
-		},
-	},
+			presets = {
+				bottom_search = true, -- Use bottom cmdline for search
+				command_palette = true, -- Position the cmdline and popupmenu together
+				long_message_to_split = true, -- Long messages will be sent to a split
+				inc_rename = false,
+				lsp_doc_border = false,
+			},
+		})
+		require("notify").setup({
+			render = "compact",
+		})
+	end,
 	dependencies = {
 		"MunifTanjim/nui.nvim",
-		-- "rcarriga/nvim-notify",
+		"rcarriga/nvim-notify",
 	},
 }
