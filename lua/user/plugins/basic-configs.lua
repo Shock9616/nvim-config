15370401787d0e9771423a36bdbd824b3e56e457
Shock9616 <Shock9616@users.noreply.github.com>
2023-10-18 22:12:07 -0700

    Lazy load plugins that aren't required at startup

--- lua/user/plugins/basic-configs.lua
+++ lua/user/plugins/basic-configs.lua
@@ -5,95 +5,96 @@
 --
 
 return {
-    {
-        -- Better commenting
-        "numToStr/Comment.nvim",
-        opts = {},
-        lazy = false,
-    },
-    {
-        -- Git gutter
-        "lewis6991/gitsigns.nvim",
-        event = { "BufReadPre", "BufNewFile" },
-        opts = {},
-    },
-    {
-        -- Support for gdscript for working in Godot
-        "habamax/vim-godot",
-        ft = "gdscript",
-    },
-    {
-        "lukas-reineke/headlines.nvim",
-        dependencies = "nvim-treesitter/nvim-treesitter",
-        ft = "markdown",
-        opts = {},
-    },
-    {
-        -- Indent guides
-        "lukas-reineke/indent-blankline.nvim",
-        config = function()
-            require("ibl").setup({
-                indent = { char = "│" },
-            })
-        end,
-    },
-    {
-        -- Live preview markdown files in browser
-        "iamcco/markdown-preview.nvim",
-        config = function()
-            vim.fn["mkdp#util#install"]()
-        end,
-        ft = "markdown",
-    },
-    {
-        -- File management as a buffer
-        "stevearc/oil.nvim",
-        config = function()
-            require("oil").setup()
-        end,
-        -- Optional dependencies
-        dependencies = { "nvim-tree/nvim-web-devicons" },
-    },
-    {
-        -- Discord rich presence because why the heck not?
-        "andweeb/presence.nvim",
-        opts = {},
-    },
-    {
-        -- Colour-matching parens
-        "https://gitlab.com/HiPhish/rainbow-delimiters.nvim.git",
-        event = { "BufReadPre", "BufNewFile" },
-        config = function()
-            vim.g.rainbow_delimiters = {}
-        end,
-    },
-    {
-        -- Better Rust support
-        "simrat39/rust-tools.nvim",
-        ft = "rust",
-    },
-    {
-        -- Epic fuzzy finder
-        "nvim-telescope/telescope.nvim",
-        tag = "0.1.2",
-        dependencies = { "nvim-lua/plenary.nvim" },
-        opts = {},
-    },
-    {
-        -- Toggleable terminal
-        "akinsho/toggleterm.nvim",
-        version = "*",
-        config = true,
-    },
-    {
-        -- Pretty diagnostics list
-        "folke/trouble.nvim",
-        opts = {},
-    },
-    {
-        -- Autopairing of parens, quotes, etc.
-        "altermo/ultimate-autopair.nvim",
-        event = { "InsertEnter", "CmdlineEnter" },
-        opts = {},
-    },
+	{
+		-- Better commenting
+		"numToStr/Comment.nvim",
+		event = { "BufReadPre", "BufNewFile" },
+		opts = {},
+	},
+	{
+		-- Git gutter
+		"lewis6991/gitsigns.nvim",
+		event = { "BufReadPre", "BufNewFile" },
+		opts = {},
+	},
+	{
+		-- Support for gdscript for working in Godot
+		"habamax/vim-godot",
+		ft = "gdscript",
+	},
+	{
+		"lukas-reineke/headlines.nvim",
+		dependencies = "nvim-treesitter/nvim-treesitter",
+		ft = "markdown",
+		opts = {},
+	},
+	{
+		-- Indent guides
+		"lukas-reineke/indent-blankline.nvim",
+		event = { "BufReadPre", "BufNewFile" },
+		config = function()
+			require("ibl").setup({
+				indent = { char = "│" },
+			})
+		end,
+	},
+	{
+		-- Live preview markdown files in browser
+		"iamcco/markdown-preview.nvim",
+		config = function()
+			vim.fn["mkdp#util#install"]()
+		end,
+		ft = "markdown",
+	},
+	{
+		-- File management as a buffer
+		"stevearc/oil.nvim",
+		config = function()
+			require("oil").setup()
+		end,
+		-- Optional dependencies
+		dependencies = { "nvim-tree/nvim-web-devicons" },
+	},
+	{
+		-- Discord rich presence because why the heck not?
+		"andweeb/presence.nvim",
+		opts = {},
+	},
+	{
+		-- Colour-matching parens
+		"https://gitlab.com/HiPhish/rainbow-delimiters.nvim.git",
+		event = { "BufReadPre", "BufNewFile" },
+		config = function()
+			vim.g.rainbow_delimiters = {}
+		end,
+	},
+	{
+		-- Better Rust support
+		"simrat39/rust-tools.nvim",
+		ft = "rust",
+	},
+	{
+		-- Epic fuzzy finder
+		"nvim-telescope/telescope.nvim",
+		tag = "0.1.2",
+		dependencies = { "nvim-lua/plenary.nvim" },
+		opts = {},
+	},
+	{
+		-- Toggleable terminal
+		"akinsho/toggleterm.nvim",
+		version = "*",
+		config = true,
+	},
+	{
+		-- Pretty diagnostics list
+		"folke/trouble.nvim",
+		opts = {},
+	},
+	{
+		-- Autopairing of parens, quotes, etc.
+		"altermo/ultimate-autopair.nvim",
+		event = { "InsertEnter", "CmdlineEnter" },
+		opts = {},
+	},
 }
