08420a5205a6f06988d4ef537f1fc5bd562dfeb2
Shock9616 <Shock9616@users.noreply.github.com>
2024-02-18 23:42:23 -0800

    Switch to <S-CR> for autocomplete

--- lua/user/config/plugins/lsp.lua
+++ lua/user/config/plugins/lsp.lua
@@ -101,7 +101,7 @@ cmp.setup({
 		["<C-f>"] = cmp.mapping.scroll_docs(4),
 		["<C-Space>"] = cmp.mapping.complete(),
 		["<C-e>"] = cmp.mapping.abort(),
-		["<CR>"] = cmp.mapping.confirm({ select = true }),
+		["<S-CR>"] = cmp.mapping.confirm({ select = true }),
 
 		-- Supertab
 		["<Tab>"] = cmp.mapping(function(fallback)
