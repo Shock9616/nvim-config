675a2e6ba43b68f1e60eeec8238481183c7a1aa5
Shock9616 <Shock9616@users.noreply.github.com>
2024-02-29 01:10:43 -0800

    Fix broken lsp lualine component

--- lua/user/config/plugins/lualine.lua
+++ lua/user/config/plugins/lualine.lua
@@ -1,8 +1,8 @@
 -- Custom Lualine component to show attached language server
 local clients_lsp = function()
-	--local bufnr = vim.api.nvim_get_current_buf()
+	local bufnr = vim.api.nvim_get_current_buf()
 
-	local clients = vim.lsp.get_clients()
+	local clients = vim.lsp.buf_get_clients(bufnr)
 	if next(clients) == nil then
 		return ""
 	end
