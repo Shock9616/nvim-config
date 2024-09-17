b181fc21830cd9cab4a9fb59168a367142d0cfbb
Shock9616 <Shock9616@users.noreply.github.com>
2024-09-17 13:26:41 -0700

    Add back presence.nvim

---
+++ lua/shock/plugins/presence.lua
@@ -0,0 +1,12 @@
+--
+-- presence.lua
+--
+-- Discord rich presence to show off that I use Neovim BTW!
+--
+
+return {
+	"andweeb/presence.nvim",
+	opts = {
+		neovim_image_text = "I use Neovim BTW!",
+	},
+}
