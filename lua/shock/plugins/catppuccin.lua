80b80041523760ec0ffe8d2139c1dfed8e53252a
Shock9616 <Shock9616@users.noreply.github.com>
2024-10-08 17:32:24 -0700

    Change floating window border colour

--- lua/shock/plugins/catppuccin.lua
+++ lua/shock/plugins/catppuccin.lua
@@ -9,8 +9,10 @@ return {
 	name = "catppuccin",
 	priority = 1000,
 	config = function()
+		local flavour = "macchiato"
+
 		require("catppuccin").setup({
-			flavour = "macchiato",
+			flavour = flavour,
 			transparent_background = true,
 			integrations = {
 				cmp = true,
@@ -26,5 +28,18 @@ return {
 			},
 		})
 		vim.cmd.colorscheme("catppuccin")
+
+		-- Customizing global highlight groups
+		local colors = require("catppuccin.palettes").get_palette(flavour)
+
+		local HlGroups = {
+			FloatBorder = {
+				fg = colors.overlay2,
+			},
+		}
+
+		for hl, color in pairs(HlGroups) do
+			vim.api.nvim_set_hl(0, hl, color)
+		end
 	end,
 }
