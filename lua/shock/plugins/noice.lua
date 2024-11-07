d6f9434bd0ec9c508f6f010999a820e2e33e7e14
Shock9616 <Shock9616@users.noreply.github.com>
2024-11-06 22:27:18 -0800

    Move noice back to bottom cmdline

--- lua/shock/plugins/noice.lua
+++ lua/shock/plugins/noice.lua
@@ -10,22 +10,31 @@ return {
 	event = "VeryLazy",
 	opts = {
 		lsp = {
-			progress = {
-				enabled = true,
-			},
 			override = {
 				["vim.lsp.util.convert_input_to_markdown_lines"] = true,
 				["vim.lsp.util.stylize_markdown"] = true,
 				["cmp.entry.get_documentation"] = true,
 			},
 		},
+		cmdline = {
+			view = "cmdline",
+			format = {
+				search_down = {
+					view = "cmdline",
+				},
+				search_up = {
+					view = "cmdline",
+				},
+				input = {
+					view = "cmdline",
+				},
+			},
+		},
 		presets = {
+			bottom_search = true,
 			long_message_to_split = true,
 			lsp_doc_border = true,
-			inc_rename = false,
-		},
-		messages = {
-			enabled = true,
+			inc_rename = true,
 		},
 		views = {
 			mini = {
