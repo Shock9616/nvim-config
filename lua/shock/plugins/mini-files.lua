6c327c0f52944978530b9ffcf629a8bf4de0f317
Shock9616 <Shock9616@users.noreply.github.com>
2025-01-27 22:48:00 -0800

    Use oil-like keybinds for mini.files

--- lua/shock/plugins/mini-files.lua
+++ lua/shock/plugins/mini-files.lua
@@ -1,7 +1,15 @@
 return {
 	"echasnovski/mini.files",
 	version = "*",
-	opts = {},
+	opts = {
+		mappings = {
+			close = "<ESC>",
+			go_in = "<CR>",
+			go_in_plus = "<S-CR>",
+			go_out = "-",
+			go_out_plus = "_",
+		},
+	},
 	keys = {
 		{
 			"<leader>fe",
