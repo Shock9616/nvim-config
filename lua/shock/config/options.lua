40a64f46d17da69da6719b8d3cb389f7a5410ee2
Shock9616 <Shock9616@users.noreply.github.com>
2025-08-18 21:40:30 -0700

    Update plugins

--- lua/shock/config/options.lua
+++ lua/shock/config/options.lua
@@ -12,9 +12,9 @@ vim.g.maplocalleader = "\\"
 vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.termguicolors = true
-vim.opt.showmode = false -- Don't show because we have a statusline
+vim.opt.showmode = false   -- Don't show because we have a statusline
 vim.opt.signcolumn = "yes" -- Always show the signcolumn
-vim.opt.wrap = false -- Disable line wrap
+vim.opt.wrap = false       -- Disable line wrap
 vim.opt.cursorline = true
 vim.opt.colorcolumn = { 81 }
 vim.opt.cmdheight = 0 -- Put statusline at the very bottom
@@ -26,27 +26,27 @@ vim.opt.backup = false
 vim.opt.undodir = ".nvim/undodir/"
 vim.opt.undofile = true
 vim.opt.undolevels = 10000
-vim.opt.confirm = true -- Confirm to save changes before exiting modified buffer
+vim.opt.confirm = true     -- Confirm to save changes before exiting modified buffer
 vim.opt.compatible = false -- Who even needs vi compatibility?
 
 vim.opt.ignorecase = true
-vim.opt.smartcase = true -- Don't ignore case with capitals
-vim.opt.smartindent = true -- Insert indents automatically
+vim.opt.smartcase = true               -- Don't ignore case with capitals
+vim.opt.smartindent = true             -- Insert indents automatically
 
-vim.opt.clipboard = "unnamedplus" -- Sync w/ system clipboard
+vim.opt.clipboard = "unnamedplus"      -- Sync w/ system clipboard
 vim.opt.completeopt = "menu,menuone,noselect"
 vim.opt.wildmode = "longest:full,full" -- Command-line completion mode
-vim.opt.inccommand = "split" -- Preview substitutions live
-vim.opt.pumblend = 0 -- Popup blend
+vim.opt.inccommand = "split"           -- Preview substitutions live
+vim.opt.pumblend = 0                   -- Popup blend
 vim.opt.sessionoptions = { "buffers", "curdir", "tabpages", "winsize" }
 vim.opt.shortmess:append({ W = true, c = true, I = true })
 
-vim.opt.expandtab = true -- Use spaces instead of tabs
-vim.opt.shiftround = true -- Round indent to multiple of shiftwidth
-vim.opt.shiftwidth = 4 -- Size of an indent
-vim.opt.tabstop = 4 -- Number of spaces tabs count for
+vim.opt.expandtab = true   -- Use spaces instead of tabs
+vim.opt.shiftround = true  -- Round indent to multiple of shiftwidth
+vim.opt.shiftwidth = 4     -- Size of an indent
+vim.opt.tabstop = 4        -- Number of spaces tabs count for
 
-vim.opt.scrolloff = 10 -- Lines of context
+vim.opt.scrolloff = 10     -- Lines of context
 vim.opt.sidescrolloff = 10 -- Columns of context
 
 vim.opt.grepformat = "%f:%l:%c:%m"
