46b1b05cfb3c16d2536d521dd7c67f5be0cb5ddb
Shock9616 <Shock9616@users.noreply.github.com>
2026-03-30 15:45:20 -0700

    Reset config for 0.12

--- init.lua
+++
@@ -1,9 +0,0 @@
---
--- Shock9616's (Kaleb Rosborough's) NeoVim Config
---
-
-require("shock.config.options") -- ~/.config/nvim/lua/shock/config/options.lua
-require("shock.config.autocmds") -- ~/.config/nvim/lua/shock/config/autocmds.lua
-require("shock.lazy") -- ~/.config/nvim/lua/shock/lazy.lua
-require("shock.config.lsp")
-require("shock.config.keymaps") -- ~/.config/nvim/lua/shock/config/keymaps.lua
