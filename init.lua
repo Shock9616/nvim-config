13c60d543696914ea0a4f8919fa3cfb895bb56cb
Shock9616 <Shock9616@users.noreply.github.com>
2026-03-30 16:00:28 -0700

    Autocmds config

--- init.lua
+++ init.lua
@@ -2,6 +2,7 @@
 -- Kaleb Rosborough's Neovim Config
 --
 
-require("config.options") -- ~/.config/nvim/lua/config/options.lua
-require("config.keymaps") -- ~/.config/nvim/lua/config/keymaps.lua
-require("config.lsp")     -- ~/.config/nvim/lua/config/lsp.lua
+require("config.options")  -- ~/.config/nvim/lua/config/options.lua
+require("config.keymaps")  -- ~/.config/nvim/lua/config/keymaps.lua
+require("config.lsp")      -- ~/.config/nvim/lua/config/lsp.lua
+require("config.autocmds") -- ~/.config/nvim/lua/config/autocmds.lua
