aca04179226c2a3b9ca43e6e2db0f306f218d72c
Shock9616 <Shock9616@users.noreply.github.com>
2026-03-30 16:05:57 -0700

    Colourscheme plugin

---
+++ plugin/00-catppuccin.lua
@@ -0,0 +1,51 @@
+--
+-- catppuccin.lua
+--
+-- Best colourscheme ever!
+--
+
+vim.pack.add({ "https://github.com/catppuccin/nvim" })
+
+local flavour = "macchiato"
+
+require("catppuccin").setup({
+    flavour = flavour,
+    transparent_background = true,
+    integrations = {
+        cmp = true,
+        gitsigns = true,
+        indent_blankline = { enabled = false },
+        mason = true,
+        treesitter = true,
+        which_key = true,
+    },
+    custom_highlights = {
+        ColorColumn = { bg = "#303446" },
+        LspInlayHint = { bg = "#1e2030" },
+    },
+})
+
+vim.cmd.colorscheme("catppuccin")
+
+-- Customize global highlight groups
+local colors = require("catppuccin.palettes").get_palette(flavour)
+
+local HlGroups = {
+    FloatBorder = {
+        fg = colors.overlay2,
+    },
+    NormalFloat = {
+        bg = colors.base,
+    },
+    FloatTitle = {
+        bg = colors.base,
+    },
+    RenderMarkdownInlineHighlight = {
+        bg = colors.yellow,
+        fg = colors.mantle
+    }
+}
+
+for hl, color in pairs(HlGroups) do
+    vim.api.nvim_set_hl(0, hl, color)
+end
