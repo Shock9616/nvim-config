e12f4ccf991a25d3bc4a976ba0af8d40a10995f6
Shock9616 <Shock9616@users.noreply.github.com>
2026-03-30 19:01:58 -0700

    Add smart-splits.nvim

---
+++ plugin/08-smart-splits.lua
@@ -0,0 +1,21 @@
+--
+-- smart-splits.lua
+--
+-- Smart integration with multiplexers
+--
+
+vim.pack.add({ "https://github.com/mrjones2014/smart-splits.nvim" })
+
+require("smart-splits").setup()
+
+-- Moving between splits
+vim.keymap.set("n", "<C-h>", require("smart-splits").move_cursor_left)
+vim.keymap.set("n", "<C-j>", require("smart-splits").move_cursor_down)
+vim.keymap.set("n", "<C-k>", require("smart-splits").move_cursor_up)
+vim.keymap.set("n", "<C-l>", require("smart-splits").move_cursor_right)
+
+-- Swapping buffers between windows
+vim.keymap.set("n", "<leader><leader>h", require("smart-splits").swap_buf_left)
+vim.keymap.set("n", "<leader><leader>j", require("smart-splits").swap_buf_down)
+vim.keymap.set("n", "<leader><leader>k", require("smart-splits").swap_buf_up)
+vim.keymap.set("n", "<leader><leader>l", require("smart-splits").swap_buf_right)
