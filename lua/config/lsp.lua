19c0f8d9900d5ebe3311376d1040a1ff1e7864b1
Shock9616 <Shock9616@users.noreply.github.com>
2026-04-07 10:13:23 -0700

    Remove lsp progress bar

--- lua/config/lsp.lua
+++ lua/config/lsp.lua
@@ -275,18 +275,3 @@ end, {
 })
 
 vim.api.nvim_create_user_command("LspInfo", ":checkhealth vim.lsp", { desc = "Alias to `:checkhealth vim.lsp`" })
-
--- Display a progress bar if supported by the terminal emulator
-vim.api.nvim_create_autocmd("LspProgress", {
-	callback = function(ev)
-		local value = ev.data.params.value
-		vim.api.nvim_echo({ { value.message or "done" } }, false, {
-			id = "lsp." .. ev.data.params.token,
-			kind = "progress",
-			source = "vim.lsp",
-			title = value.title,
-			status = value.kind ~= "end" and "running" or "success",
-			percent = value.percentage,
-		})
-	end,
-})
